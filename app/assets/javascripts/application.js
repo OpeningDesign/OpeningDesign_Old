77822486da273caa7c30952723f39c470c940459
Hemant Verma <fameoflight@gmail.com>
2012-11-03 13:56:00 +0530

    Fixing Javascript Error and Moving Unnecessary js libraries to load after the page has been loaded.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,20 +4,6 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+// hemantv : Only the absolute required javascript should be here to optimize run times
 //= require jquery
 //= require jquery_ujs
-//= require jquery.gritter
-//= require jquery-ui-1.8.16.custom.min
-//= require jquery.fileupload
-//= require jquery.simplemodal-1.4.1
-//= require jquery.validate
-//= require jquery.charCount
-//= require jquery.best_in_place
-//= require jquery.tooltip
-//= require jquery.tagsinput
-//= require bootstrap
-//= require bootstrap-tab
-//= require ember
-//= require app/app
-//= require app/upload
-//= require_tree .
---
+++ app/assets/javascripts/application_others.js
@@ -0,0 +1,17 @@
+// Other than important javascript should be loaded here
+//
+//= require jquery.gritter
+//= require jquery-ui-1.8.16.custom.min
+//= require jquery.fileupload
+//= require jquery.simplemodal-1.4.1
+//= require jquery.validate
+//= require jquery.charCount
+//= require jquery.best_in_place
+//= require jquery.tooltip
+//= require jquery.tagsinput
+//= require bootstrap
+//= require bootstrap-tab
+//= require ember
+//= require app/app
+//= require app/upload
+//= require_tree .
